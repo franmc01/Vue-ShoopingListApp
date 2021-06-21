3e10f7d2f852524acf7ae2fd336c9ab1c4c2aecf
franciscomarin98 <francmarinc@gmail.com>
2021-06-21 15:30:02 -0500

    Haciendo limpieza

--- index.js
+++ index.js
@@ -4,15 +4,8 @@ const app = Vue.createApp({
             header: 'Shopping List App',
             newItem: '',
             editing: false,
-            newItemPriority: 'low',
             newItemHighPriority: false,
-            iceCreamFlavors: [],
             arrayItems: [],
-            objectItems: {
-                'item-1': { id: 1, label: '10 party hats', purchased: false },
-                'item-2': { id: 2, label: '2 board games', purchased: true },
-                'item-3': { id: 3, label: '20 cups', purchased: true },
-            }
         }
     },
     methods: {
@@ -44,9 +37,3 @@ const app = Vue.createApp({
     },
 
 }).mount('#shopping-list');
-
-
-// Apuntes
-/* Para agregar clases dinamicas en Vue existen dos formas - Object Sintax y Array Syntax,
-cabe mencionar que no es obligatorio que se deba aprender ambas, en todo caso cualquiera de
-dos cumple su cometido; en lo personal yo me quedo con la de Array Syntax */
