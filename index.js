3d9c3bbbb8cc80b5a189474c45cdbdb20ef3d84a
franciscomarin98 <francmarinc@gmail.com>
2021-06-21 11:26:08 -0500

    Reforzando el uso de l v-if

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = Vue.createApp({
         return {
             header: 'Shopping List App',
             newItem: '',
+            editing: false,
             newItemPriority: 'low',
             newItemHighPriority: false,
             iceCreamFlavors: [],
             arrayItems: [
-                { id: 1, label: '10 party hats' },
-                { id: 2, label: '2 board games' },
-                { id: 3, label: '20 cups' },
+                // { id: 1, label: '10 party hats' },
+                // { id: 2, label: '2 board games' },
+                // { id: 3, label: '20 cups' },
             ],
             objectItems: {
                 'item-1': { id: 1, label: '10 party hats' },
@@ -22,6 +23,10 @@ const app = Vue.createApp({
         saveItem() {
             this.arrayItems.push({ id: this.arrayItems.length + 1, label: this.newItem });
             this.newItem = "";
+        },
+        doEdit(editing) {
+            this.editing = editing;
+            this.newItem = '';
         }
     }
 
